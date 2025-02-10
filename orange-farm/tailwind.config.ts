1423b890215ad90663c4df6abaa664d451c5a5ef
David Irinyemi <iowoyemidavid12@gmail.com>
2025-02-10 22:54:45 +0100

    configured project colors

--- orange-farm/tailwind.config.ts
+++ orange-farm/tailwind.config.ts
@@ -9,8 +9,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        offwhite: "#f6f6f3",
+        orange: "#ffa500",
+        midgray: "#7d7d7d",
+        darkorange: "#734a00",
+        textblack: "131313",
+        lightorange: "#FFF7E8",
+        lightgray: "#F1F1F1",
+        darkgray: "#F1F1F1",
+        lightgreen: "#25CD25",
+        red: "#EB5050",
+        midorange: "#FFCD71",
+        darkred: "B01212"
+
       },
     },
   },
