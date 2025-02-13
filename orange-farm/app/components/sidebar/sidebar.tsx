12c0454e64defbb69fd8186c2927443b42c0e3d1
David Irinyemi <iowoyemidavid12@gmail.com>
2025-02-13 01:06:49 +0100

    styled sidebar

--- orange-farm/app/components/sidebar/sidebar.tsx
+++ orange-farm/app/components/sidebar/sidebar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import logo from "../../icons/Logo.svg"
 import searchicon from "../../icons/search.svg"
+import dashboardIcon from"../../icons/dashboard-icon.svg"
 
 
 export default function Sidebar() {
@@ -9,11 +10,20 @@ export default function Sidebar() {
       <div>
         <Image src={logo} alt="Orange-farm-logo" width={100} height={100} priority className="w-40" />
 
-        <div className="border flex p-4 gap-2 rounded-full items-center">
+        {/* Search bar wrapper */}
+        <div className="border flex px-4 py-2 mt-10 gap-2 rounded-full items-center">
           <Image src={searchicon} alt="search-icon" width={30} height={30} className="h-6 w-6" / >
           <input placeholder ="Search" className="w-full text-lg"/>
+        </div>
+
+        {/* Menu button wrapper */}
+        <div className="mt-10">
+          <button className="flex">
+            <Image src={dashboardIcon} alt="dashvoard-icon" width={10} height={10} className="w-6"/>
+            Dashboard
+          </button>
         </div>
       </div>
     </div>
   );
-}
+} 
