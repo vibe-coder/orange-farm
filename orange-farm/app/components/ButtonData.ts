4f6c9c096ce2ddb401247e4767b07ce8866ded3d
vibe-coder <iowoyemidavid12@gmail.com>
2025-02-28 22:10:29 +0100

    Created a Data file for dynamic button component

---
+++ orange-farm/app/components/ButtonData.ts
@@ -0,0 +1,17 @@
+
+
+interface dataStructure {
+    name: string,
+    url: string
+}
+
+const Data:dataStructure[] = [
+    {
+        name: "Dashboard",
+        url: "../componnets/icons/dashboard-icon.svg"},
+    {
+        name: "Customers",
+        url: "../componnets/icons/dashboard-icon.svg"
+    }
+
+]
