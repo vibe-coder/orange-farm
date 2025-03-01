e79f3530dedb18f9fd295d3eba1de8c432207e50
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-01 16:16:46 +0100

    Corrected iconUrls to import from the public folder

--- orange-farm/app/components/ButtonData.ts
+++ orange-farm/app/components/ButtonData.ts
@@ -1,17 +1,52 @@
+import dashboardIcon from "../icons/dashboard-icon.svg"
+import newIcon from "../../public/icons/dashboard-icon.svg"
+
 
 
 interface dataStructure {
+    index: number,
     name: string,
     url: string
 }
 
 const Data:dataStructure[] = [
     {
+        index: 1,
         name: "Dashboard",
-        url: "../componnets/icons/dashboard-icon.svg"},
+        url: "../../public/icons/dashboard-icon.svg",
+    },
     {
+        index: 2,
         name: "Customers",
-        url: "../componnets/icons/dashboard-icon.svg"
+        url: "../../public/icons/customers.svg"
+    },
+    {
+        index: 3,
+        name: "All reports",
+        url: "../../public/icons/reports.svg"
+    },
+    {
+        index: 4,
+        name: "Geography",
+        url: "../../public/icons/geography.svg"
+    },
+    {
+        index: 5,
+        name: "Conversations",
+        url: "../../public/icons/conversations.svg"
+    },
+    {
+        index: 6,
+        name: "Deals",
+        url: "../../public/icons/deals.svg"
+    },
+    {
+        index: 7,
+        name: "Exports",
+        url: "../../public/icons/exports.svg"
     }
 
 ]
+
+
+export default Data;
