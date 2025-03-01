b1834612c899e3827839268e85b5bb87ef89b680
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-01 17:53:34 +0100

    imported footer component

--- orange-farm/app/page.tsx
+++ orange-farm/app/page.tsx
@@ -1,14 +1,19 @@
-import Sidebar from "./components/sidebar/sidebar";
-
+import Footer from "./components/footer";
+import Sidebar from "./sidebar/sidebar";
 
 
 
 
 export default function Home() {
   return (
-    <div className="bg-offwhite text-black p-4">
-      <main className="h-screen">
-        <Sidebar/>
+    <div className="bg-offwhite text-black">
+      <main className="h-lvh pl-4 pt-4 bg-lightgreen  bg-red">
+        
+        <div className="bg-darkorange mb-2">
+          <Sidebar/>
+        </div>
+
+        <Footer/>
       </main>
     </div>
   );
