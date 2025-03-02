ef8a5a4e93498dcb4b59250e83bad72879047275
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-02 22:27:41 +0100

    set main wrapper width to full

--- orange-farm/app/dashboard/page.tsx
+++ orange-farm/app/dashboard/page.tsx
@@ -1,10 +1,35 @@
-
+import Image from "next/image"
+import greenIcon from "../../public/icons/Vector.svg"
 
 
 export default function Dashboard(){
     return(
-        <div className="flex-none w-80">
-            This is a div
+        <div className="flex-none w-full">
+            {/* Top */}
+            <div className="flex">
+                {/* Revenues */}
+                <div className="bg-white flex-1">
+                    <h1>Revenues</h1>
+                    <div>
+                        <div className="flex">
+                            <h1>51%</h1>
+                            <Image src={greenIcon} alt="arrow" width={50} />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Lost deals */}
+            </div>
+
+            {/* Middele */}
+            <div>
+                This is for middle
+            </div>
+
+            {/* Bottom */}
+            <div>
+                This is for bottom
+            </div>
         </div>
     )
 }
