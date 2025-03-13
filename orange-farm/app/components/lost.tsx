100bbd4ef27db7530b7c39b0463692ebf2fb2b27
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-13 21:26:44 +0100

    Chaged percentage summary element colour to dark gray

--- orange-farm/app/components/lost.tsx
+++ orange-farm/app/components/lost.tsx
@@ -11,10 +11,10 @@ export default function Lostdeals(){
                     <h1 className="text-5xl font-medium">5%</h1>
                     
                 </div>
-                <p className="text-sm pt-[-20px]">You closed 96 out of 100 deals</p>
+                <p className="text-sm pt-[-20px]  text-darkgray">You closed 96 out of 100 deals</p>
             </div>
 
-            <button className="flex text-darkorange text-sm gap-2 ">
+            <button className="flex text-darkorange text-sm gap-2 absolute bottom-4">
                 All deals
                 <Image src={darkorangearrow} alt="forward-arrow" width={50} className="w-3"/>
             </button>
--- orange-farm/app/components/revenues.tsx
+++ orange-farm/app/components/revenues.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image"
 import greenIcon from "../../public/icons/Vector.svg"
 import darkorangearrow from "../../public/icons/darkorangearrow.svg"
+import { Luckiest_Guy } from "next/font/google"
 
 
 
 
 export default function Revenues(){
     return(
-        <div className=" bg-darkorange p-4 rounded-2xl h-60 gap-4 grid-row-3 relative ">
+        <div className="bg-white p-4 flex-col rounded-2xl h-60 gap-4 relative ">
             <h1 className="font-semibold text-lg text-textblack pb-8">Revenues</h1>
 
             <div>
@@ -15,10 +16,10 @@ export default function Revenues(){
                     <h1 className="text-5xl font-medium">15%</h1>
                     <Image src={greenIcon} alt="arrow" width={50} className="w-4"/>
                 </div>
-                <p className="text-sm">Increase compared to last week</p>
+                <p className="text-sm pt-[-20px] text-darkgray">Increase compared to last week</p>
             </div>
 
-            <button className="flex absolute bottom-3 place-self-end bg-red text-darkorange text-sm gap-2">
+            <button className="flex text-darkorange text-sm gap-2 absolute bottom-4">
                 Revenues report
                 <Image src={darkorangearrow} alt="forward-arrow" width={50} className="w-3"/>
             </button>
