b73f9ca8151e726a1b21597246036ce5a0cff5f6
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-13 21:21:40 +0100

    Created lost deals component

---
+++ orange-farm/app/components/lost.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import darkorangearrow from "../../public/icons/darkorangearrow.svg"
+
+export default function Lostdeals(){
+    return(
+        <div className="bg-white p-4 flex-col rounded-2xl h-60 gap-4 relative ">
+            <h1 className="font-semibold text-lg text-textblack pb-8">Lost deals</h1>
+
+            <div>
+                 <div className="flex gap-4 pb-4">
+                    <h1 className="text-5xl font-medium">5%</h1>
+                    
+                </div>
+                <p className="text-sm pt-[-20px]">You closed 96 out of 100 deals</p>
+            </div>
+
+            <button className="flex text-darkorange text-sm gap-2 ">
+                All deals
+                <Image src={darkorangearrow} alt="forward-arrow" width={50} className="w-3"/>
+            </button>
+        </div>
+    )
+}
