2e39ab29aa64e3c12893d0a64f0beb6374d1b4a7
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-17 19:47:56 +0100

    Included key attribute

--- orange-farm/app/components/CustomerData.ts
+++ orange-farm/app/components/CustomerData.ts
@@ -1,6 +1,7 @@
 
 
 interface Profile {
+    key: number,
     profilePictureURL: string,
     name: string,
     company: string
@@ -9,22 +10,26 @@ interface Profile {
 
 const customerData: Profile[]  = [
     {
+        key: 1,
         profilePictureURL: '/icons/Chris.png',
         name: 'Christ Friedkly',
         company: 'Supermarket Villanova'
     },
 
     {
+        key: 2,
         profilePictureURL: '/icons/Maggie.png',
         name: 'Maggie Johnson',
         company: 'Oasis Organic Inc.'
     },
     {
+        key: 3,
         profilePictureURL: '/icons/Harry.png',
         name: 'Gael Harry',
         company: 'Newyork Finest Fruits'
     },
     {
+        key: 4,
         profilePictureURL: '/icons/Jenna.png',
         name: 'Jenna Sullivan',
         company: 'Wallmart'
