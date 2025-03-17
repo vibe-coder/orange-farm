9e9eabb80ddf79cc747aa0f4632e9498dbf39ede
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-17 14:02:05 +0100

    Created customer data file

---
+++ orange-farm/app/components/CustomerData.ts
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+interface Profile {
+    profilePictureURL: string,
+    name: string,
+    company: string
+}
+
+
+const customerData: Profile[]  = [
+    {
+        profilePictureURL: 'unknown',
+        name: 'Christ Friedkly',
+        company: 'Supermarket Villanova'
+    },
+
+    {
+        profilePictureURL: 'unknown',
+        name: 'Maggie Johnson',
+        company: 'Oasis Organic Inc.'
+    },
+    {
+        profilePictureURL: 'unknown',
+        name: 'Gael Harry',
+        company: 'Newyork Finest Fruits'
+    },
+    {
+        profilePictureURL: 'unkknown',
+        name: 'Jenna Sullivan',
+        company: 'Wallmart'
+    }
+]
+
+
+export default customerData
