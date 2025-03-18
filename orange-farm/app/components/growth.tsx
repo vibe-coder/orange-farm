e713b171f4b6afdcb9bcb3d6d148c9672b8a64e4
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-18 09:17:45 +0100

    created growth component

---
+++ orange-farm/app/components/growth.tsx
@@ -0,0 +1,17 @@
+import TimeCustomizedMenus from "./timecustomizedmenu"
+
+
+export default function Growth(){
+    return(
+        <div className="bg-white rounded-2xl gap-4 p-4">
+            <div className="flex justify-between">
+                <h1 className="text-lg font-bold">Growth</h1>
+                <TimeCustomizedMenus/>
+            </div>
+
+            <div>
+                
+            </div>
+        </div>
+    )
+}
