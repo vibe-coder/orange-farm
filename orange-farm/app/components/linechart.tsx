e37e3f6407270659f2eba5ac7364f2e3371045b0
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-18 11:04:56 +0100

    configured line chart to UI specifications

--- orange-farm/app/components/linechart.tsx
+++ orange-farm/app/components/linechart.tsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
-import { LineChart } from '@mui/x-charts/LineChart';
+import { LineChart, lineElementClasses } from '@mui/x-charts/LineChart';
 
 export default function BasicArea() {
   return (
     <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+      xAxis={[{ data: [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023,] }]}
       series={[
         {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
+            curve: "linear",
+          data: [1, 2, 5, 6, 2, 3, 6, 7,],
           area: true,
         },
       ]}
-      width={500}
+      width={450}
       height={300}
+      grid={{ vertical: true, horizontal: true }}
+      yAxis={[{
+        colorMap: {
+            type: 'continuous',
+            min: -10,
+            max: 20,
+            color: ['transparent', '#25CD25'],
+          }
+      }]}
+      sx={{
+        [`& .${lineElementClasses.root}`]: {
+          strokeDasharray: '10 5',
+          strokeWidth: 4,
+        }
+      }}
+      
     />
   );
 }
