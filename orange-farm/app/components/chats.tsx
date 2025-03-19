ffc7803ce84e732fd19552037cced1c0f820cb22
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-19 11:12:44 +0100

    created chats component

---
+++ orange-farm/app/components/chats.tsx
@@ -0,0 +1,12 @@
+
+
+
+
+export default function Chats(){
+    return(
+        <div className="bg-white p-4">
+            <h1 className="text-textblack font-bold text-lg">Chats</h1>
+            <p className="text-darkgray text-sm">2 unread messages</p>
+        </div>
+    )
+}
