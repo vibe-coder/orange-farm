df4e4fcdb7d9fd56d2dc8e0a0d63bd0030f3321c
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-19 17:41:25 +0100

    added button element

--- orange-farm/app/components/chats.tsx
+++ orange-farm/app/components/chats.tsx
@@ -1,20 +1,24 @@
 "use client"
 
 import BadgeAvatars from "./avatarbadge"
-
-
+import { customerWithMessage } from "./chatData"
+import Image from "next/image"
+import darkorangearrow from "../../public/icons/darkorangearrow.svg"
 
 export default function Chats(){
     return(
-        <div className="bg-white p-4">
+        <div className="bg-white p-4 rounded-2xl">
             <h1 className="text-textblack font-bold text-lg">Chats</h1>
-            <p className="text-darkgray text-sm">2 unread messages</p>
+            {<p className="text-darkgray text-sm"> {customerWithMessage.length} unread messages</p>}
 
-            <div className="pt-8">
-                <div>
-                    <BadgeAvatars/>
-                </div>
+            <div className="pt-8 mb-8">
+                <BadgeAvatars/>
             </div>
+
+            <button className="flex text-darkorange text-sm gap-2 p-2">
+                All Customers
+                <Image src={darkorangearrow} alt="forward-arrow" width={50} className="w-3"/>
+            </button>
         </div>
     )
 }
