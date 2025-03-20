24b0b5c82257eafe46aa5c6e46badb49fe0bf3d2
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-20 18:17:43 +0100

    created New Deal Data

---
+++ orange-farm/app/components/newDealData.ts
@@ -0,0 +1,44 @@
+
+
+
+interface NewDeal {
+    key: number,
+    name: string
+}
+
+
+const newDealData: NewDeal[] = [
+    {
+        key: 1,
+        name: 'Fruit2Go'
+    },
+    {
+        key: 2,
+        name: "Marshall's MKT"
+    },
+    {
+        key: 3,
+        name: 'CCNT'
+    },
+    {
+        key: 4,
+        name: 'Joana Mini-market'
+    },
+    {
+        key:5,
+        name: 'Little Brazil Vegan'
+    },
+    {
+        key: 6,
+        name: 'Target'
+    },
+    {
+        key: 7,
+        name: 'Organic Place'
+    },
+    {
+        key: 8,
+        name: "Morello's"
+    }
+
+]
