2da5ba9a035dcc465dc74c482a8352f866fba8a9
vibe-coder <iowoyemidavid12@gmail.com>
2025-03-26 21:55:14 +0100

    removed unnecessary styling

--- orange-farm/app/page.tsx
+++ orange-farm/app/page.tsx
@@ -7,13 +7,15 @@ import Dashboard from "./dashboard/page";
 export default function Home() {
   return (
     <div className="bg-offwhite text-black w-full h-lvh overflow-hidden">
-      <main className="pl-4 py-4 h-full overflow-scroll flex gap-4">
-          <div className=" overflow-scroll w-92 flex-none">
+      <main className="h-full overflow-scroll no-scrollbar relative">
+          <div className=" overflow-scroll h-full p-4 w-full flex-none fixed ">
             <Sidebar/>
           </div>
 
-          <div className="overflow-scroll w-full flex-none pr-4">
-            <Dashboard/>
+          <div className=" no-scrollbar w-[190%] flex">
+            <div className="w-screen overflow-y-scroll no-scrollbar z-10 p-4 bg-offwhite">
+              <Dashboard/>
+            </div>
           </div>
       </main>
     </div>
