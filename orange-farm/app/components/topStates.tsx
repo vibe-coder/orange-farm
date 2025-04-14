9aa5566cfdf49d7ef43e0f0e58eb3b7fe466d270
vibe-coder <iowoyemidavid12@gmail.com>
2025-04-14 16:39:44 +0100

    imported barchart component

--- orange-farm/app/components/topStates.tsx
+++ orange-farm/app/components/topStates.tsx
@@ -1,11 +1,15 @@
+import BarChart from "./barChart"
 
 
 
 
 export default function TopStates(){
     return(
-        <div className="bg-white">
+        <div className="bg-white p-4 rounded-2xl">
             <h1 className="text-lg font-bold">Top States</h1>
+            <div>
+                <BarChart/>
+            </div>
         </div>
     )
 }
