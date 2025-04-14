a70b631b122a33f8a5d64f52f91c39a443653ea6
vibe-coder <iowoyemidavid12@gmail.com>
2025-04-14 16:38:39 +0100

    Replaced old sidebar with swipe sidebar and removed unnecessary style atrribute

--- orange-farm/app/page.tsx
+++ orange-farm/app/page.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "./sidebar/sidebar";
 import Dashboard from "./dashboard/page";
+import SwipeSidebar from "./sidebar/swipesidebar";
 
 
 
@@ -8,13 +9,11 @@ export default function Home() {
   return (
     <div className="bg-offwhite text-black w-full h-lvh overflow-hidden">
       <main className="h-full overflow-scroll no-scrollbar relative">
-          <div className=" overflow-scroll h-full p-4 w-full flex-none fixed ">
-            <Sidebar/>
-          </div>
+          <SwipeSidebar/>
 
           <div className=" overflow-x-scroll  snap-center scroll-smooth snap-x 
-          no-scrollbar snap-mandatory w-[190%] flex ">
-            <div className=" z-30 w-[190%] flex justify-end">
+          no-scrollbar snap-mandatory flex ">
+            <div className=" flex justify-end">
               <div className="w-screen no-scrollbar z-20 p-4 bg-offwhite">
                 <Dashboard/>
               </div>
