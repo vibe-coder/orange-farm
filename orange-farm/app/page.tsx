a49d1975e782b1dfc6bfdb83c08bc79586ec7f96
vibe-coder <iowoyemidavid12@gmail.com>
2025-04-15 19:22:16 +0100

    Wrapped header in a different container and removed usused style attributes

--- orange-farm/app/page.tsx
+++ orange-farm/app/page.tsx
@@ -1,20 +1,26 @@
 import Sidebar from "./sidebar/sidebar";
 import Dashboard from "./dashboard/page";
 import SwipeSidebar from "./sidebar/swipesidebar";
+import Header from "./components/header"
+import ResponsiveAppBar from "./components/header";
 
 
 
 
 export default function Home() {
   return (
-    <div className="bg-offwhite text-black w-full h-lvh overflow-hidden">
+    <div className="bg-offwhite text-black w-screen h-lvh overflow-hidden">
       <main className="h-full overflow-scroll no-scrollbar relative">
           <SwipeSidebar/>
+          
+          <div className="bg-white py-2">
+            <ResponsiveAppBar/>
+          </div>
 
-          <div className=" overflow-x-scroll  snap-center scroll-smooth snap-x 
+          <div className=" overflow-y-scroll  snap-center scroll-smooth snap-x 
           no-scrollbar snap-mandatory flex ">
             <div className=" flex justify-end">
-              <div className="w-screen no-scrollbar z-20 p-0 bg-offwhite">
+              <div className=" no-scrollbar z-20 p-0 bg-offwhite">
                 <Dashboard/>
               </div>
             </div>
