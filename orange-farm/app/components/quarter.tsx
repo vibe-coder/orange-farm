7c005afe8378f5e8270b46357211d94c804f2e07
vibe-coder <iowoyemidavid12@gmail.com>
2025-04-26 19:08:30 +0100

    removed unused css lines

--- orange-farm/app/components/quarter.tsx
+++ orange-farm/app/components/quarter.tsx
@@ -8,7 +8,7 @@ import BasicGauges from "./gauge"
 
 export default function Quartergoals(){
     return(
-        <div className="bg-white col-span-2 flex-col p-4 p-4 rounded-2xl h-60 gap-4 relative">
+        <div className="bg-white col-span-2 p-4 rounded-2xl h-60 gap-4 relative">
             <h1 className="font-semibold text-lg text-textblack text-center">Quarter goal</h1>
 
             <div className="h-40 flex justify-center items-center">
